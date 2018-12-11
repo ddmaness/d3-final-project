243fc5fa4997160d9de6e76e72653e1535cf9e16
Donald Maness <ddmaness@gmail.com>
2018-12-11 13:07:41 -0500

    Add tooltips for plot points

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ function draw(data) {
 
     const svg = d3.select('body')
         .append('svg')
-        .attr('width', width + margin)
+        .attr('width', width + margin + 200)
         .attr('height', height + margin)
         .append('g')
         .attr('class', 'chart');
@@ -19,6 +19,12 @@ function draw(data) {
         .enter()
         .append('circle');
 
+    d3.select('body')
+        .selectAll('div')
+        .data(data)
+        .enter()
+        .append('div');
+    
     const hrExtent = d3.extent(data, function(d){
         return d.hr;
     });
@@ -51,6 +57,45 @@ function draw(data) {
         .attr('transform', 'translate( ' + margin + ', 0)')
         .call(hrAxis);
 
+    d3.selectAll('div')
+        .attr('class', 'info-panel')
+        .attr('id', function(d) {
+            return d.name.split(' ').join('-');
+        })
+        .style('top', function(d) {
+            return ((hrScale(d.hr))+ 50) + 'px';
+        })
+        .style('left', function(d) {
+            return ((weightScale(d.weight)) + 20) + 'px';
+        });
+    
+    d3.selectAll('div')
+        .append('h3')
+        .attr('class', 'info-panel-name');
+    
+    d3.selectAll('div')
+        .append('p')
+        .attr('class', 'info-panel-weight');
+
+    d3.selectAll('div')
+        .append('p')
+        .attr('class', 'info-panel-hr');
+    
+    d3.selectAll('.info-panel-name')
+        .text(function(d){
+            return d.name;
+        });
+
+    d3.selectAll('.info-panel-weight')
+        .text(function(d){
+            return 'Weight: ' + d.weight + 'lbs';
+        });
+
+    d3.selectAll('.info-panel-hr')
+        .text(function(d){
+            return 'Career homers: ' + d.hr;
+        });
+    
     d3.selectAll('circle')
         .attr('cx', function(d) {
             return weightScale(d.weight);
@@ -58,17 +103,20 @@ function draw(data) {
         .attr('cy', function(d) {
             return hrScale(d.hr);
         })
-        .attr('r', function(d) {
-            return radius;
+        .attr('r',radius)
+        .on('mouseover', function(d){
+            document.getElementById(d.name.split(' ').join('-')).classList.add('is-visible');
+        })
+        .on('mouseout', function(d){
+            document.getElementById(d.name.split(' ').join('-')).classList.remove('is-visible');
         })
         .attr('class', 'point');
 }
-
 
 d3.csv('./data/baseball_data.csv', function(d) {
     return {
         name: d.name,
         weight: +d.weight,
         hr: +d.HR
-    }
+    };
 }).then(draw);
